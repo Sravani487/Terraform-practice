06c260fd4249bb05898b894fbe2615195297d047
CloudDevops487 <sravani.v487@gmail.com>
2025-11-04 20:33:15 +0530

    Day-8

---
+++ Day-3-State-file/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id  = "ami-0bdd88bd06d16ba03" #add required ami id
+instance_type = "t3.micro"       #add required instance type
+availability_zone = "us-east-1a" #add requied az
---
+++ Day-3-State-file/variables.tf
@@ -0,0 +1,20 @@
+variable "ami_id" {
+    description = "Passing the ami values"
+    default = ""
+    type = string
+  
+}
+
+variable "instance_type" {
+    description = "passing the instance type"
+    default = ""
+    type = string
+  
+}
+
+variable "availability_zone" {
+    description = "Passing az value"
+    default = ""
+    type = string
+  
+}
